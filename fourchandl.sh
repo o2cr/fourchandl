ef390a7e0b0dde5948cf8e468893ec8ebe4333b4
yucachaaaan <harris.prism@gmail.com>
2013-12-09 05:20:24 +0000

    Update fourchandl.sh

--- fourchandl.sh
+++ fourchandl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 fourchan () {
         if ["${1}" == "help" && "${2}" == "pls"]; then
-        printf "Usage: fourchan <page URL> <folder name>\n"
+            printf "Usage: fourchan <page URL> <folder name>\n"
             return 1;
         fi
-        mkdir ${2} && cd ${2} && $(curl -s ${1} | grep -o -i 'File: <a href="//i.   4cdn.org\/[a-z]*\/src\/[0-9]*\.[a-z]\{3\}' | sed -r 's/File: <a href="\/\///'   |xargs wget) && cd ..
+        mkdir ${2} && cd ${2} && $(curl -s ${1} | grep -o -i 'File: <a href="//i.4cdn.org\/[a-z]*\/src\/[0-9]*\.[a-z]\{3\}' | sed -r 's/File: <a href="\/\///'   |xargs wget) && cd ..
 }
